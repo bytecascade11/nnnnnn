5eebc98db91f79ae4d28f29b6e24da19ed307cb4
bytecascade11 <okesunday503@gmail.com>
2025-12-13 07:25:08 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,12 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://revibyte.blog',
+
+  vite: {
+    build: {
+      rollupOptions: {
+        external: ['virtual:astro-icon'],
+      },
+    },
+  },
 });
