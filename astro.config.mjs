78653c3b861d8aa5da1ab1a594225434dac58db5
bytecascade11 <okesunday503@gmail.com>
2025-12-13 10:03:55 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,6 @@
 import { defineConfig } from 'astro/config';
-import icon from '@astrojs/icon';  // Keep this!
-
-// Your other imports...
+import icon from 'astro-icon';
 
 export default defineConfig({
-  integrations: [
-    icon(),  // Keep this!
-    // ...your other integrations (e.g., tailwind, etc.)
-  ],
+  integrations: [icon()],
 });
